6dd9ca0719791ebbccee5f19f962b6f844320524
Obichukwu Onyeowuzoni <obichukwu.zoni@gmail.com>
2020-12-27 04:27:35 +0100

    Added HomePage and PostPage. Added BlogLayout

---
+++ web/src/pages/HomePage/HomePage.js
@@ -0,0 +1,12 @@
+import { Link, routes } from '@redwoodjs/router'
+import BlogLayout from 'src/layouts/BlogLayout'
+
+const HomePage = () => {
+  return (
+    <BlogLayout>
+      <p>Taking Fullstack to the Jamstack</p>
+    </BlogLayout>
+  )
+}
+
+export default HomePage
